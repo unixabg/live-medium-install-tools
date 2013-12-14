4cdfc192d311764a80b698f307e1c3707b0a0b6c
Richard Nelson <unixabg@gmail.com>
2013-12-14 11:07:17 -0600

    Minor format cleanup on vcs page.

--- vcs.php
+++ vcs.php
@@ -2,10 +2,10 @@
 include "header.php";
 ?>
 <body>
-        <div id="conholder">
-                <div id="maincon">
-                        <h1 class="texth1">Version Control System</h1>
-                        <h4 class="texth4">Live-Medium-Install is currently included in the debian-next branch of <a href="http://live.debian.net/gitweb/?p=live-tools.git">live-tools<a/></h4>
+	<div id="conholder">
+		<div id="maincon">
+			<h1 class="texth1">Version Control System</h1>
+			<h4 class="texth4">Live-Medium-Install is currently included in the debian-next branch of <a href="http://live.debian.net/gitweb/?p=live-tools.git">live-tools<a/></h4>
 		</div>
 	</div>
 <?php
