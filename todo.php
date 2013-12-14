18104b289882632b518e9cb61b04c8a19232f7e7
Richard Nelson <unixabg@gmail.com>
2013-12-14 11:06:36 -0600

    Minor format cleanup on todo page.

--- todo.php
+++ todo.php
@@ -2,11 +2,10 @@
 include "header.php";
 ?>
 <body>
-        <div id="conholder">
-                <div id="maincon">
-                        <h1 class="texth1">To Do</h1>
-                        <h4 class="texth4">
-			</h4>	
+	 <div id="conholder">
+		<div id="maincon">
+			<h1 class="texth1">To Do</h1>
+			<h4 class="texth4"></h4>
 		</div>
 	</div>
 <?php
