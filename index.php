fe4be1e0795197e46151678f8eea43274d5aca5f
Richard Nelson <unixabg@gmail.com>
2013-12-14 10:54:39 -0600

    Initial update of index page.

--- index.php
+++ index.php
@@ -6,8 +6,10 @@ include "header.php";
 		<div id="maincon">
 			<h1 class="texth1">Live-Medium-Install</h1>
 			<h4 class="texth4">
-			This page is a placeholder for live-medium-install setups live-medium-install is intended to serve as a firmware install of debian-live media. <br />
-			Live-medium-install is currently included in the live-tools.git debian-next branch.
+			<ul>
+				<li>This page is a placeholder for live-medium-install setups live-medium-install is intended to serve as a firmware install of debian-live media. </li>
+				<li>Live-medium-install is currently included in the live-tools.git debian-next branch.</li>
+			</ul>
 			</h4>
 		</div>
 	</div>
