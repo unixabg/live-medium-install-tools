45ab56ddf0508cd5973f5576039d6c06fad4f73d
Richard Nelson <unixabg@gmail.com>
2013-12-14 10:56:00 -0600

    Fix html on vcs page.

--- vcs.php
+++ vcs.php
@@ -5,7 +5,7 @@ include "header.php";
         <div id="conholder">
                 <div id="maincon">
                         <h1 class="texth1">Version Control System</h1>
-                        <h4 class="texth4">live-medium-install is currently included in the debian-next branch of <a href="http://live.debian.net/gitweb/?p=live-tools.git">live-tools<a/>
+                        <h4 class="texth4">live-medium-install is currently included in the debian-next branch of <a href="http://live.debian.net/gitweb/?p=live-tools.git">live-tools<a/></h4>
 		</div>
 	</div>
 <?php
