e69b2bc7fb51a5bff457e8af0158499dc7f7ceb6
Richard Nelson <unixabg@gmail.com>
2014-01-01 14:03:58 -0600

    Update todo.php

--- todo.php
+++ todo.php
@@ -5,7 +5,17 @@ include "header.php";
 	 <div id="conholder">
 		<div id="maincon">
 			<h1 class="texth1">To Do</h1>
-			<h4 class="texth4"></h4>
+			<h4 class="texth4">
+			<p> Updated 20140101. </p>
+				<ul>
+					<li>Improve firmware injection to use rsync</li>
+					<li>Improve firmware injection to use locking</li>
+					<li>Improve firmware injection to use test for updates for single instance runs</li>
+					<li>Improve partial-squashfs-updates to include changelog or notes of patch</li>
+					<li>Setup README</li>
+					<li>Consider Makefile install</li>
+				</ul>
+			</h4>
 		</div>
 	</div>
 <?php
