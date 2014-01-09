3c4f62846a786ca29863fdb70635074d128396be
Richard Nelson <unixabg@gmail.com>
2014-01-09 13:40:32 -0600

    Include README file on index.php.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include "header.php";
 			<h1 class="texth1">Live-Medium-Install</h1>
 			<h4 class="texth4">
 			<ul>
-				<li>This page is a placeholder for <b>live-medium-install</b> setups live-medium-install is intended to serve as a firmware install of debian-live media. </li>
+				<li>This page is a placeholder for <b>live-medium-install</b> setups live-medium-install is intended to serve as a firmware install of debian-live media. To get started examine the <a href="README">README</a> file.</li>
 				<li><b>live-medium-install</b> is currently included in the live-tools.git debian-next branch.</li>
 			</ul>
 			</h4>
