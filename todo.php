eb3a5ef403e2ec9a3dac9b34a9d2c7ca5d286aeb
Richard Nelson <unixabg@gmail.com>
2014-01-09 13:38:44 -0600

    Updated todo.php.

--- todo.php
+++ todo.php
@@ -6,15 +6,19 @@ include "header.php";
 		<div id="maincon">
 			<h1 class="texth1">To Do</h1>
 			<h4 class="texth4">
-			<p> Updated 20140101. </p>
+			<p> Updated 20140109. </p>
 				<ul>
+					<li>Consider swapfile and persistence options</li>
+					<li>Update VCS to have a direct link to latest live-media-install script</li>
 					<li>Improve firmware injection to use rsync</li>
 					<li>Improve firmware injection to use locking</li>
 					<li>Improve firmware injection to use test for updates for single instance runs</li>
 					<li>Improve partial-squashfs-updates to include changelog or notes of patch</li>
-					<li>Setup README</li>
+					<li><strike>Setup README</strike></li>
 					<li>Consider Makefile install</li>
+					<li></li>
 				</ul>
+			</p>
 			</h4>
 		</div>
 	</div>
