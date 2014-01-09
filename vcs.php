62dce7b378240bc133bb3981a1984baf55b2cc99
Richard Nelson <unixabg@gmail.com>
2014-01-09 13:49:16 -0600

    Update link in vcs.php file.

--- vcs.php
+++ vcs.php
@@ -5,7 +5,9 @@ include "header.php";
 	<div id="conholder">
 		<div id="maincon">
 			<h1 class="texth1">Version Control System</h1>
-			<h4 class="texth4"><b>live-medium-install</b> is currently included in the debian-next branch of <a href="http://live.debian.net/gitweb/?p=live-tools.git">live-tools<a/></h4>
+			<h4 class="texth4">
+			<a href="http://live.debian.net/gitweb/?p=live-tools.git;a=blob_plain;f=bin/live-medium-install;hb=refs/heads/debian-next"><b>live-medium-install</b></a> is currently in the debian-next branch of <a href="http://live.debian.net/gitweb/?p=live-tools.git">live-tools<a/>
+			</h4>
 		</div>
 	</div>
 <?php
