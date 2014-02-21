75891fbfe047d33c8ca0887a8a3b264f76c39922
Richard Nelson <unixabg@gmail.com>
2014-02-21 12:23:06 -0600

    Update index.php for live-medium-install-tools information.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ include "header.php";
 <body>
 	<div id="conholder">
 		<div id="maincon">
-			<h1 class="texth1">Live-Medium-Install</h1>
+			<h1 class="texth1">Live-Medium-Install-Tools</h1>
 			<h4 class="texth4">
 			<ul>
-				<li>This page is a placeholder for <b>live-medium-install</b> setups live-medium-install is intended to serve as a firmware install of debian-live media. To get started examine the <a href="README">README</a> file.</li>
+				<li>This site is a placeholder for information and tools realted to <b>live-medium-install</b> setups. The <b>live-medium-install</b> script is intended to serve as a firmware install of debian-live media. To get started examine the <a href="README">README</a> file.</li>
 				<li><b>live-medium-install</b> is currently included in the live-tools.git debian-next branch.</li>
 			</ul>
 			</h4>
