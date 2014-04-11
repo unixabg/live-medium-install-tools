5961d262afecd766630e3ffaf12aa2ce77efa5ac
Richard Nelson <unixabg@gmail.com>
2014-04-11 09:37:35 -0500

    Added git information for the live-medium-install-tools in vcs.php.

--- vcs.php
+++ vcs.php
@@ -9,6 +9,8 @@ include "header.php";
 			<a href="http://live.debian.net/gitweb/?p=live-tools.git;a=blob_plain;f=bin/live-medium-install;hb=refs/heads/debian-next"><b>live-medium-install</b></a> is currently in the debian-next branch of <a href="http://live.debian.net/gitweb/?p=live-tools.git">live-tools<a/>
 			<br>
 			<a href="http://live.debian.net/gitweb/?p=live-tools.git;a=blob_plain;f=bin/live-partial-squashfs-updates;hb=refs/heads/debian-next"><b>live-partial-squashfs-updates</b></a> is currently in the debian-next branch of <a href="http://live.debian.net/gitweb/?p=live-tools.git">live-tools<a/>
+			<br>
+			<a href="https://github.com/unixabg/live-medium-install-tools.git"><b>live-medium-install-tools</b></a> is currently at <a href="http://github.com">github.com<a/>
 			</h4>
 		</div>
 	</div>
