37a1db4127d18469d19d3b94dfccaf0563b276dd
Richard Nelson <unixabg@gmail.com>
2014-04-26 14:34:17 -0500

    Add some videos to videos.php.

--- videos.php
+++ videos.php
@@ -6,6 +6,16 @@ include "header.php";
 		<div id="maincon">
 			<h1 class="texth1">Videos</h1>
 			<h4 class="texth4">
+			<p><b>Example of live-medium-install.</b></p>
+			<iframe width="420" height="345"
+			src="http://www.youtube.com/embed/uhLG6J1DHeM">
+			</iframe>
+
+			<p><b>Example of custom-live-medium-install.</b></p>
+			<iframe width="420" height="345"
+			src="http://www.youtube.com/embed/ti0rdf3RChI">
+			</iframe>
+
 			</h4>
 		</div>
 	</div>
