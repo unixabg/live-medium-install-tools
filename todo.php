47721dccddf5b6d9c0eddd7ff0feb82302318cf9
Richard Nelson <unixabg@gmail.com>
2014-05-25 19:56:11 -0500

    Update todo.php

--- todo.php
+++ todo.php
@@ -6,9 +6,11 @@ include "header.php";
 		<div id="maincon">
 			<h1 class="texth1">To Do</h1>
 			<h4 class="texth4">
-			<p> Updated 20140413. </p>
+			<p> Updated 20140525. </p>
 				<ul>
+					<li>Update Makefile for server side setup</li>
 					<li>Consider swapfile <strike>and persistence options</strike></li>
+					<li><strike>Consider local live medium for install source</strike></li>
 					<li><strike>Update VCS to have a direct link to latest live-media-install script</strike></li>
 					<li><strike>Create firmware.updates to rsync for partial.squashfs updates.</strike></li>
 					<li><strike>Improve firmware session injection to use test for updates for single instance runs</strike></li>
