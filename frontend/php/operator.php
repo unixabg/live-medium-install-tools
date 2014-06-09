c567149afac114c27c82643f3984619eeefdd017
johnburd <johnburd19@gmail.com>
2014-06-09 08:38:05 -0500

    Added operator.php frontend/php

---
+++ frontend/php/operator.php
@@ -0,0 +1,12 @@
+<?php
+$dir = "./machines/";
+$mac = $_GET['mac'];
+$machine_dir = scandir("./machines/$mac");
+$count_machine = count($machine_dir);
+for ($i = 2; $i < $count_machine; $i++) {
+	if ($machine_dir[$i] != "info.txt") {
+		echo "$machine_dir[$i] ";
+	}
+}
+
+?>
