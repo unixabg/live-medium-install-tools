d65369d1e7bc2861d464b1d718850c296d2dc2f6
Richard Nelson <unixabg@gmail.com>
2014-06-09 11:45:26 -0500

    Minor adjustment on symlink from library to scripts folder onfrontend/php.

--- frontend/php/admin_changes.php
+++ frontend/php/admin_changes.php
@@ -5,7 +5,8 @@ $count = count($library);
 for ($x = 2; $x < $count; $x++) {
 	$post = $_POST[$library[$x]];
 	if ($post == "1") {
-		symlink("./library/$library[$x]", "./scripts/$library[$x]");
+		// Step up one dir from anticipated symlink target.
+		symlink("../library/$library[$x]", "./scripts/$library[$x]");
 	} elseif($post != "1") {
 	unlink("./scripts/$library[$x]");
 	}
