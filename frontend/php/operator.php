5c09ef5ccf7b42d19f7ebec4b4aeee61fed1a98d
johnburd <johnburd19@gmail.com>
2014-06-10 16:09:14 -0500

    Fixed blank mac address search in operator.php frontend/php.

--- frontend/php/operator.php
+++ frontend/php/operator.php
@@ -1,11 +1,12 @@
 <?php
-$mac = $_GET['mac'];
-$machine_dir = scandir("./machines/$mac");
-$count_machine = count($machine_dir);
-for ($i = 2; $i < $count_machine; $i++) {
-	if ($machine_dir[$i] != "info.txt") {
-		echo "$machine_dir[$i] ";
+if(!empty($_GET['mac'])) {
+	$mac = $_GET['mac'];
+	$machine_dir = scandir("./machines/$mac");
+	$count_machine = count($machine_dir);
+	for ($i = 2; $i < $count_machine; $i++) {
+		if ($machine_dir[$i] != "info.txt") {
+			echo "$machine_dir[$i] ";
+		}
 	}
 }
-
 ?>
