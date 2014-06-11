2b169ea9e290f8ad54620c77ffc524c41d13d77e
johnburd <johnburd19@gmail.com>
2014-06-11 11:09:57 -0500

    Added functions for table header and checks in table frontend/php.

---
+++ frontend/php/functions.php
@@ -0,0 +1,28 @@
+<?php
+# scans $dir and prints each item formated into table header, excludes . and .. file.
+function scan_th($dir) {
+	$dir = scandir($dir);
+	$count = count($dir);
+	for ($x = 0; $x < $count; $x++) {
+		if ($dir[$x] != "." && $dir[$x] != "..") {
+			echo "<th class=\"e\">".$dir[$x]."</th>";
+		}
+	}
+}
+
+# scans $file for files it contains and scans $dir to print out each item with a check or no check, excludes . and .. file.
+function check_box($file, $dir) {
+	$dir = scandir($dir);
+	$count = count($dir);
+	for ($loop_count = 0; $loop_count < $count; $loop_count++) {
+		if ($dir[$loop_count] != "." && $dir[$loop_count] != "..") {
+			if (is_link("./machines/"."/".$file."/".$dir[$loop_count]) || is_file("./machines/".$file."/".$dir[$loop_count])) {
+				echo "<td class=\"e\">&#10003;</td>";
+			} else {
+				echo "<td class=\"e\"></td>";
+			}
+		}
+	}
+}
+
+?>
