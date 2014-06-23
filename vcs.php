734ecac879fdd80dcfb768f4ec0f4a942b84d7cd
Richard Nelson <unixabg@gmail.com>
2014-06-22 21:30:57 -0500

    Adjust vcs links while live-tools adjusts for scripts.

--- vcs.php
+++ vcs.php
@@ -6,10 +6,6 @@ include "header.php";
 		<div id="maincon">
 			<h1 class="texth1">Version Control System</h1>
 			<h4 class="texth4">
-			<a href="http://live.debian.net/gitweb/?p=live-tools.git;a=blob_plain;f=bin/live-medium-install;hb=refs/heads/debian-next"><b>live-medium-install</b></a> is currently in the debian-next branch of <a href="http://live.debian.net/gitweb/?p=live-tools.git">live-tools<a/>
-			<br>
-			<a href="http://live.debian.net/gitweb/?p=live-tools.git;a=blob_plain;f=bin/live-partial-squashfs-updates;hb=refs/heads/debian-next"><b>live-partial-squashfs-updates</b></a> is currently in the debian-next branch of <a href="http://live.debian.net/gitweb/?p=live-tools.git">live-tools<a/>
-			<br>
 			<a href="https://github.com/unixabg/live-medium-install-tools.git"><b>live-medium-install-tools</b></a> is currently at <a href="http://github.com">github.com<a/>
 			</h4>
 		</div>
