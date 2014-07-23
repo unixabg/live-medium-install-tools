feb954d18be66b01f42fb26077bb368458043fb6
johnburd <johnburd19@gmail.com>
2014-07-23 12:46:06 -0500

    Added log file system to each machine frontend/php.

--- frontend/php/operator.php
+++ frontend/php/operator.php
@@ -13,10 +13,22 @@ if(!empty($_GET['mac'])) {
 				//echo $count_scripts;
 				for ($i = 0; $i < $count_scripts; $i++) {
 					if ($mac_scripts[$i] != "." && $mac_scripts[$i] != "..") {
-						if ($mac_scripts[$i] != "info.txt") {
+						if ($mac_scripts[$i] != "info.txt" && $mac_scripts[$i] != "log.txt") {
 							echo "$group_match/$mac/$mac_scripts[$i] ";
+							$scripts .= " $mac_scripts[$i]";
 						}
 					}
+				}
+				$log = "./machines/$group_match/$mac/log.txt";
+				if (!is_file($log)) {
+					touch($log);
+				}
+				file_put_contents($log, "[".date("Y-m-d H:i:s")."]\tscripts: $scripts\n", FILE_APPEND);
+				$linecount = count(file($log));
+				if ($linecount > 250) {
+					$file = file($log);
+					unset($file[0]);
+					file_put_contents($log, $file);
 				}
 			}
 		}
