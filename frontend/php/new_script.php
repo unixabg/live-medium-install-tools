4aa87f5665139f6a073adbab4033c1663cc35a0b
johnburd <johnburd19@gmail.com>
2014-08-15 13:23:36 -0500

    Cleaned up new line returns for bash scripts frontend/php.

--- frontend/php/new_script.php
+++ frontend/php/new_script.php
@@ -1,5 +1,7 @@
 <?php
 $new_script = $_POST['new_script'];
+$new_script = preg_replace("/\r\n/", "\n", $new_script); // DOS style newlines
+$new_script = preg_replace("/\r/", "\n", $new_script); // Mac newlines for nostalgia
 $file = $_POST['file'];
 $dir = $_POST['dir'];
 file_put_contents("$dir/$file", $new_script);
