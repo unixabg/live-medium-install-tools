6211063c6fcb4184ad1ee343f9d8067296107374
Richard Nelson <unixabg@gmail.com>
2014-12-13 20:32:27 -0600

    Added missing <html> open tag on frontend/php/header.php.

--- frontend/php/header.php
+++ frontend/php/header.php
@@ -9,6 +9,7 @@
 */
 ?>
 <!DOCTYPE html>
+<html>
 <script>
 //if (window.location.protocol != "https:")
 //    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
